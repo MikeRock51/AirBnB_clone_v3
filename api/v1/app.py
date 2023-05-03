e75214e58ad2f468c69d90c40bb7d2564e78bc6b
MikeRock51 <mikerockmusic51@gmail.com>
2023-05-03 12:34:38 +0100

    Add task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""
-A flask web app that runs on 0.0.0.0:5000
-"""
+'''A flask web app that runs on 0.0.0.0:5000'''
 
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -16,6 +14,12 @@ app.register_blueprint(app_views)
 def tearDown(self):
     """Removes the current SQLAlchemy Session"""
     storage.close()
+
+
+@app.errorhandler(404)
+def notFound(error):
+    """Returns a JSON formatted not found response"""
+    return make_response(jsonify({'error': 'Not found'}))
 
 
 if __name__ == '__main__':
