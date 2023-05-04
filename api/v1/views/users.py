53a1beb18dcfee94ef33581e041fa1d68924551c
MikeRock51 <mikerockmusic51@gmail.com>
2023-05-04 10:43:26 +0100

    Add User POST/PUT methods

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -35,3 +35,36 @@ def deleteUser(user_id):
         abort(404)
     storage.delete(user)
     return make_response(jsonify({}), 200)
+
+
+@app_views.route('/users', methods=['POST'])
+def createUser():
+    """Creates a new User object"""
+    userInfo = request.get_json()
+    if type(userInfo) != dict:
+        make_response(jsonify({"error": "Not a JSON"}), 400)
+    elif 'email' not in userInfo:
+        return make_response(jsonify({"error": "Missing email"}), 400)
+    elif 'password' not in userInfo:
+        return make_response(jsonify({"error": "Missing password"}), 400)
+
+    user = User(**userInfo)
+    user.save()
+    return make_response(jsonify(user.to_dict()), 201)
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'])
+def updateUser(user_id):
+    """Updates a User object with given user_id"""
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    userInfo = request.get_json()
+    if type(userInfo) != dict:
+        make_response(jsonify({"error": "Not a JSON"}), 400)
+    ignoredKeys = ['id', 'email', 'created_at', 'updated_at']
+    for key, value in userInfo.items():
+        if key not in ignoredKeys:
+            setattr(user, key, value)
+    user.save()
+    return make_response(jsonify(user.to_dict()), 200)
